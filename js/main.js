2d5b143ec329cfb10ea87068dd1e08a8ac9e0652
Sabrina Bueno <bina_fbueno@hotmail.com>
2023-04-27 16:13:41 -0300

    forEach

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 const subtrair = document.querySelector("#subtrair") 
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
-const controle = document.querySelectorAll("controle-ajuste")
+const controle = document.querySelectorAll(".controle-ajuste")
 
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
 
-somar.addEventListener("click",() => {manipulaDados("somar")})
-
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")}) 
+})
 
 
 function manipulaDados(operacao) {
-    if(operacao === "subtrair") {
+    if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1;
     } else {
         braco.value = parseInt(braco.value) + 1;
