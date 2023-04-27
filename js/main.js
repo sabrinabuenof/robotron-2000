e076472cd8075fdeb285ec71f77a9af650f3a14a
Sabrina Bueno <bina_fbueno@hotmail.com>
2023-04-27 15:39:38 -0300

    functions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-const robotron = document.querySelector("#robotron") 
+const subtrair = document.querySelector("#subtrair") 
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll("controle-ajuste")
 
 
-robotron.addEventListener("click", () => {
-    console.log("Cliquei no robô.")
-})
+somar.addEventListener("click",() => {manipulaDados("somar")})
 
-function dizOi (nome) {
-    console.log("Oi " + nome)
-    console.log("Bem-vindo ao Robotron 2000")
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")}) 
+
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
 }
 
-dizOi("Sabrina");
